0e576bd7c69c8301a949d2387182c6cf45fe6c6e
Schuh93 <daniel.schuh200493@gmail.com>
2025-11-13 19:46:01 +0100

    add text_node_to_html_node and tests for it

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,11 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
-    CODE_TEXT = "code"
+    CODE = "code"
     LINK = "link"
     IMAGE = "image"
 
@@ -24,3 +25,27 @@ class TextNode():
         
     def __repr__(self):
         return f"{self.__class__.__name__}({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(tag=None, value=text_node.text)
+
+        case TextType.BOLD:
+            return LeafNode(tag="b", value=text_node.text)
+
+        case TextType.ITALIC:
+            return lnode.LeafNode(tag="i", value=text_node.text)
+
+        case TextType.CODE:
+            return LeafNode(tag="code", value=text_node.text)
+        
+        case TextType.LINK:
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        
+        case TextType.IMAGE:
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
+
+        case _:
+            raise ValueError(f"invalid TextType: {text_node.text_type}")
