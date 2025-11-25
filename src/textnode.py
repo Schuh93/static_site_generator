0d0c0e9af0d6dcd840c17ea8dbe1846a1e651927
Schuh93 <daniel.schuh200493@gmail.com>
2025-11-25 17:42:41 +0100

    add markdown_to_html_node and tests for it

--- src/textnode.py
+++ src/textnode.py
@@ -36,7 +36,7 @@ def text_node_to_html_node(text_node):
             return LeafNode(tag="b", value=text_node.text)
 
         case TextType.ITALIC:
-            return lnode.LeafNode(tag="i", value=text_node.text)
+            return LeafNode(tag="i", value=text_node.text)
 
         case TextType.CODE:
             return LeafNode(tag="code", value=text_node.text)
